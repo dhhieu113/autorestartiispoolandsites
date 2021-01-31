0c0a9bb2161fd91de76587244bc36e45fe3e8f88
Hieu Dam (SM) <hdam@deloitte.ca>
2021-01-31 14:02:32 +0700

    firstInit

---
+++ IISAutoRestartApp/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace IISAutoRestartApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblcurrent = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 156);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(415, 282);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Stopped IIS Pools";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(468, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Stopped Websites";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(446, 156);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(342, 282);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(143, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Update Check Interval";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // lblcurrent
+            // 
+            this.lblcurrent.AutoSize = true;
+            this.lblcurrent.Location = new System.Drawing.Point(332, 27);
+            this.lblcurrent.Name = "lblcurrent";
+            this.lblcurrent.Size = new System.Drawing.Size(56, 17);
+            this.lblcurrent.TabIndex = 6;
+            this.lblcurrent.Text = "Hiện tại";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblcurrent);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auto Restart IIS Pool & Websites";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblcurrent;
+    }
+}
+
---
+++ IISAutoRestartApp/Form1.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.Web.Administration;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace IISAutoRestartApp
+{
+    public partial class Form1 : Form
+    {
+        private int checkInterval = 5000;
+        private List<string> notActivePools = new List<string>();
+        private List<string> notActiveWebsites = new List<string>();
+        ServerManager server = new ServerManager();
+        private string poolLogFiles = "pools-check.txt";
+        private string websiteLogFiles = "websites-check.txt";
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+            timer1.Start();
+            var intervalStr = (checkInterval / 1000).ToString();
+            textBox1.Text = intervalStr;
+            lblcurrent.Text = "Hiện tại: " + intervalStr + "s";
+        }
+
+        private void LoadStopedPools()
+        {
+            // notActivePools = new List<string>();
+
+            foreach (var pool in server.ApplicationPools)
+            {
+                try
+                {
+                    if (pool.State == ObjectState.Stopped)
+                    {
+                        var msg = pool.Name + $" was stopped at {DateTime.Now}";
+                        notActivePools.Add(msg);
+                        LogPool(msg);
+                        pool.Start();
+                        var startMsg = $"{pool.Name} is started at {DateTime.Now}";
+                        LogPool(startMsg);
+                        notActivePools.Add(startMsg);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    LogPool(ex.Message);
+                }
+            }
+
+            richTextBox1.Text = string.Join("\n", notActivePools);
+        }
+
+        private void LoadStoppedWebsites()
+        {
+            //notActiveWebsites = new List<string>();
+            foreach (var web in server.Sites)
+            {
+                try
+                {
+                    if (web.State == ObjectState.Stopped)
+                    {
+                        var msg = web.Name + $" was stopped at {DateTime.Now}";
+                        notActiveWebsites.Add(msg);
+                        LogWebsite(msg);
+                        web.Start();
+                        var startMsg = $"{web.Name} is started at {DateTime.Now}";
+                        LogWebsite(startMsg);
+                        notActiveWebsites.Add(startMsg);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    LogWebsite(ex.Message);
+                }
+            }
+
+            richTextBox2.Text = string.Join("\n", notActiveWebsites);
+        }
+
+        private void LogPool(string msg)
+        {
+            System.IO.File.AppendAllText(poolLogFiles, "\n" + msg);
+        }
+
+        private void LogWebsite(string msg)
+        {
+            System.IO.File.AppendAllText(websiteLogFiles, "\n" + msg);
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            LoadStopedPools();
+            LoadStoppedWebsites();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                checkInterval = int.Parse(textBox1.Text) * 1000;
+                var intervalStr = (checkInterval / 1000).ToString();
+                lblcurrent.Text = "Hiện tại: " + intervalStr + "s";
+
+                timer1.Stop();
+                timer1.Interval = checkInterval;
+                timer1.Start();
+            }
+            catch
+            {
+                MessageBox.Show("Interval là giá trị từ 1 - 99, đơn vị: s (giây)");
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer1.Stop();
+            timer1.Enabled = false;
+            timer1.Dispose();
+        }
+    }
+}
